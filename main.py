4f6ca327cbcb92602baa48c1e757e0c2cfb450aa
Gsus <jago2811@gmail.com>
2023-07-13 11:48:45 -0600

    [add] cors policy

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from routes.statistics import statistics
 app = FastAPI()
 
 app.include_router(statistics)
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Probability & Statistics API"}
 
