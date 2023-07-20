242f047d5a8a97f63a77d6edcad61b99ea268eb6
Gsus <jago2811@gmail.com>
2023-07-20 00:46:53 -0600

    [fix]

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.statistics import statistics
 
 app = FastAPI()
 
+# Configuración de CORS para permitir las solicitudes desde el origen especificado
 origins = ["https://aquaqa.sytes.net"]
-
-
-@app.middleware("http")
-async def add_cors_headers(request: Request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = ",".join(origins)
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
-    return response
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluye tu enrutador `statistics` en la aplicación
 app.include_router(statistics)
-
 
 @app.get("/")
 async def root():
