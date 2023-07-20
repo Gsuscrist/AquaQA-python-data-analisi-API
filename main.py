e634ff03db8ab0dfb78459134e6b7aa7176413e8
Gsus <jago2811@gmail.com>
2023-07-20 00:35:30 -0600

    [fix]

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from routes import statistics
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
+from routes.statistics import statistics
 
 app = FastAPI()
 
 origins = ["https://aquaqa.sytes.net"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = ",".join(origins)
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+    return response
+
+
+# Incluye tu enrutador `statistics` en la aplicación
 app.include_router(statistics)
 
 
